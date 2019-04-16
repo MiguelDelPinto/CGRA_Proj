bb4dd96227093fa91d4537ded557785e103a6951
Pedro Esteves <up201705160@fe.up.pt>
2019-04-16 20:48:42 +0100

    Added window to MyHouse

--- WebCGF2.projA/projA/MyHouse.js
+++ WebCGF2.projA/projA/MyHouse.js
@@ -12,6 +12,8 @@ class MyHouse extends CGFobject {
 
         this.pillar = new MyPrism(scene, 6, 1);
         this.wrap_pillar = new MyPrism(scene, 6, 1, true);
+
+        this.window = new MyQuad(scene);
                 
         this.initMaterials();
         this.initBuffers();
@@ -29,6 +31,7 @@ class MyHouse extends CGFobject {
     initMaterials(){
         this.roof_texture = new CGFtexture(this.scene, 'images/roof.jpg');
  	    this.pillar_texture = new CGFtexture(this.scene, 'images/pillar.jpg');
+		this.window_texture = new CGFtexture(this.scene, 'images/window.jpg');
 
     	//Roof Material
         this.roof_material = new CGFappearance(this.scene);
@@ -45,14 +48,34 @@ class MyHouse extends CGFobject {
         this.pillar_material.setSpecular(0.1, 0.1, 0.1, 1);
         this.pillar_material.setShininess(10.0);
         this.pillar_material.setTexture(this.pillar_texture);
+
+        //Window Material
+        this.window_material = new CGFappearance(this.scene);
+        this.window_material.setAmbient(0.1, 0.1, 0.1, 1);
+        this.window_material.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.window_material.setSpecular(0.1, 0.1, 0.1, 1);
+        this.window_material.setShininess(10.0);
+        this.window_material.setTexture(this.window_texture);
     }
     
     display() {
 		//Drawing Walls
-		//this.wall_material.apply();
 		this.scene.pushMatrix();
 		this.scene.translate(0, 0.5, 0);
+		this.scene.pushMatrix();
+		this.scene.scale(3, 1, 1);
 		this.walls.display();
+		this.scene.popMatrix();
+		this.scene.popMatrix();
+
+		//Drawing Window
+		this.window_material.apply();
+		this.scene.pushMatrix();
+		this.scene.scale(0.5, 0.5, 1);
+		this.scene.pushMatrix();
+		this.scene.translate(0, 1, 0.501);
+		this.window.display();
+		this.scene.popMatrix();
 		this.scene.popMatrix();
 
 		//Drawing Roof
@@ -60,7 +83,7 @@ class MyHouse extends CGFobject {
 		this.scene.pushMatrix();
 		this.scene.rotate(Math.PI/4, 0, 1, 0);
 		this.scene.pushMatrix();
-		this.scene.scale(1.5, 1, 1.5);
+		this.scene.scale(2.5, 1, 2.5);
 		this.scene.pushMatrix();
 		this.scene.translate(0, 1, 0);
 		if(this.scene.wrapTextures)
@@ -76,7 +99,7 @@ class MyHouse extends CGFobject {
 		this.pillar_material.apply();
 		
 		this.scene.pushMatrix();
-		this.scene.translate(0.75, 0, 0.75);
+		this.scene.translate(1.5, 0, 1.25);
 		this.scene.pushMatrix();
 		this.scene.scale(0.25, 1, 0.25);
 		this.pillar.display();
@@ -84,7 +107,7 @@ class MyHouse extends CGFobject {
 		this.scene.popMatrix();
 
 		this.scene.pushMatrix();
-		this.scene.translate(-0.75, 0, 0.75);
+		this.scene.translate(-1.5, 0, 1.25);
 		this.scene.pushMatrix();
 		this.scene.scale(0.25, 1, 0.25);
 		if(this.scene.wrapTextures)
@@ -96,7 +119,7 @@ class MyHouse extends CGFobject {
 		this.scene.popMatrix();
 
 		this.scene.pushMatrix();
-		this.scene.translate(-0.75, 0, -0.75);
+		this.scene.translate(-1.5, 0, -1.25);
 		this.scene.pushMatrix();
 		this.scene.scale(0.25, 1, 0.25);
 		if(this.scene.wrapTextures)
@@ -108,7 +131,7 @@ class MyHouse extends CGFobject {
 		this.scene.popMatrix();
 
 		this.scene.pushMatrix();
-		this.scene.translate(0.75, 0, -0.75);
+		this.scene.translate(1.5, 0, -1.25);
 		this.scene.pushMatrix();
 		this.scene.scale(0.25, 1, 0.25);
 		if(this.scene.wrapTextures)
