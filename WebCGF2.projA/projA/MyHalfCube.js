12a36fb94ed58b2640a92115607a51773275a290
Pedro Esteves <up201705160@fe.up.pt>
2019-04-17 19:03:39 +0100

    Added other faces to MyHalfCube

--- WebCGF2.projA/projA/MyHalfCube.js
+++ WebCGF2.projA/projA/MyHalfCube.js
@@ -70,6 +70,28 @@ class MyHalfCube extends CGFobject {
 		this.scene.popMatrix();
 		this.scene.popMatrix();
 		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(0, -1, 0);
+		this.scene.pushMatrix();
+		this.scene.rotate(Math.PI/2, 1, 0, 0);
+		this.scene.pushMatrix();
+		this.scene.scale(2, 1, 1);
+		this.quad.display();
+		this.scene.popMatrix();
+		this.scene.popMatrix();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(-1, 0, 0);
+		this.scene.pushMatrix();
+		this.scene.rotate(-Math.PI/2, 0, 1, 0);
+		this.scene.pushMatrix();
+		this.scene.scale(1, 2, 1);
+		this.quad.display();
+		this.scene.popMatrix();
+		this.scene.popMatrix();
+		this.scene.popMatrix();
 		
 	}
 }
